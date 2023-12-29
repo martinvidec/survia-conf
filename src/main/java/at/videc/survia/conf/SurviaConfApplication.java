f8546a168d17671c46feedd09f2f192a26eb773d
martinvidec <rx451g@gmail.com>
2023-12-29 21:40:47 +0100

    Initial commit

---
+++ src/main/java/at/videc/survia/conf/SurviaConfApplication.java
@@ -0,0 +1,13 @@
+package at.videc.survia.conf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurviaConfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurviaConfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/videc/survia/conf/SurviaConfApplicationTests.java
@@ -0,0 +1,13 @@
+package at.videc.survia.conf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurviaConfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
